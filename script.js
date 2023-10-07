6cea04b89bd4593b673ee38c76a1f6c77c09546f
Jesca Ryanne Acosta <89693266+jescaryanneacosta@users.noreply.github.com>
2023-10-07 16:26:43 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function signinpage(){
+    window.location.href="signin.html"
+}
+function signuppage(){
+    window.location.href="signup.html"
+}
+function signupalert(){
+    alert("Sign-up Successful! Welcome to Taste Taft")
+}
+function feed(){
+    window.location.href="feed.html";
+}
+function feedguest(){
+    window.location.href="feed-guest.html";
+}
+function userpage(){
+    window.location.href="useraccount.html"
+}
+function sampleres1(){
+    window.location.href="sample-res1.html"
+}
+
+
+function disableScroll() {
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        window.onscroll = function() {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+function enableScroll() {
+    window.onscroll = function() {};
+}
+
+
