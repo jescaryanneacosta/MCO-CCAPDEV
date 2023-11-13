cb7a0dceae19ee35b09acd63dd7de485c9b17596
Miguel <93895318+bendItLikeBeckham@users.noreply.github.com>
2023-11-13 08:25:37 +0800

    connected the website to node.js
    made subdirectory for the html,css, images and such then made the local host link work to run the website

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
-
+const path = require('path');
 const app = express();
 const port = 3000;
 const url = "mongodb://localhost:27017/persons";
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 mongoose.connect(url, {})
     .then(result => console.log("database connected"))
     .catch(err => console.log(err))
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'signin.html'));
+});
 
 mongoose.connect(url)
 app.listen(port, () =>
 {console.log("server is running at port" + port)}
 )
-app.get('/', (req, res) => {
-    res.send("<h1>Hello from nodejs app</h1>")
-});
 
 
 /*import { connectToMongo } from "./mongo models/conn.js";
