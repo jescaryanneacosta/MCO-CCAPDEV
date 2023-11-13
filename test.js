aad17cd0781a24f6bb40d2421a93e195da60036a
Miguel <93895318+bendItLikeBeckham@users.noreply.github.com>
2023-11-13 14:23:12 +0800

    a

--- test.js
+++ test.js
@@ -2,13 +2,13 @@ const assert = require('assert');
 const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 
-const User = require('../MCO-CCAPDEV/mongo model/user.model'); // Import your MongoDB model
+const User = require('../MCO-CCAPDEV/mongo model/user.model'); 
 
 let mongoServer;
 
 before(async () => {
   mongoServer = new MongoMemoryServer();
-  await mongoServer.start(); // Ensure that the server is started
+  await mongoServer.start(); 
   const mongoUri = await mongoServer.getUri();
   await mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
 });
@@ -20,17 +20,13 @@ after(async () => {
 
 describe('User Model', () => {
   it('should store values in MongoDB', async () => {
-    // Arrange
     const testData = {
       username: "Bellingham",
       password: "zinedinezidane"
-      // Add other properties if needed, but make sure they match your User model
     };
 
-    // Act
     const result = await User.create(testData);
 
-    // Assert
     assert.strictEqual(result.username, testData.username);
     assert.strictEqual(result.password, testData.password);
     assert.strictEqual(result.role, 'User');
@@ -40,7 +36,6 @@ describe('User Model', () => {
     assert.strictEqual(result.likes.length, 0); 
     assert.strictEqual(result.dislikes.length, 0); 
 
-    // Check if the user was stored in the database
     const storedUser = await User.findOne({ username: testData.username });
     assert(storedUser, 'User not found in the database');
     assert.strictEqual(storedUser.username, testData.username);
